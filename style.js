9b611dfdc8ffbbab0a66a8daa75e51a9282a8bb0
Tracy Gwehona <tracy.gwehona@gmail.com>
2025-03-03 17:42:31 +0300

    Edit

--- style.js
+++
@@ -1,8 +0,0 @@
-// Add event listeners to each portfolio item
-document.querySelectorAll('.portfolio-header').forEach(item => {
-    item.addEventListener('click', function() {
-      const parent = this.closest('.portfolio-item');
-      parent.classList.toggle('active');
-    });
-  });
-  
